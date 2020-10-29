f4c0f3a76670b90d34567450521a02640585548b
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-29 00:30:30 -0400

    deployed and styled

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Badge, Card,CardBody,CardHeader} from 'reactstrap'
 const ToDo = (props) => {
 
     const handleClick = () => {
@@ -7,7 +7,14 @@ const ToDo = (props) => {
     }
     return (
         <div onClick={handleClick} className={`item${props.task.completed?" completed" :""}`}>
-           <p>{props.task.task}</p>
+            <div>
+                <Card color="primary" style={{width:'25%'}}>
+                    <CardBody>
+                        <Badge>{props.task.id}</Badge>
+                        <CardHeader style={{fontWeight:'bold'}}>{props.task.task}</CardHeader>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     )
 }
